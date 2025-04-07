21cec514961bbae0cb4c3062dce39da3acc84320
SlinkyGoat <adamustby@gmail.com>
2025-04-07 18:49:30 -0500

    can turn tv on./off

--- flaskr/tv_controller.py
+++ flaskr/tv_controller.py
@@ -1,13 +1,23 @@
 import subprocess
 
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+bp = Blueprint('tv_controls', __name__, url_prefix='/tv-controls')
+
 is_tv_on = False
 
-def tv_turn_on():
+@bp.route('/turn_on', methods=('GET', 'POST'))
+def turn_on():
     # subprocess.run(["echo", "on 0", "|", "cec-client", "-s", "-d", "1"])
     subprocess.run(["echo on 0 | cec-client -s -d 1"], shell=True)
     is_tv_on = True
+    return ''
 
-def tv_turn_off():
+@bp.route('/turn_off', methods=('GET', 'POST'))
+def turn_off():
     # subprocess.run(["echo", "standby 0", "|", "cec-client", "-s", "-d", "1"])
     subprocess.run(["echo standby 0 | cec-client -s -d 1"], shell=True)
     is_tv_on = False
+    return ''
